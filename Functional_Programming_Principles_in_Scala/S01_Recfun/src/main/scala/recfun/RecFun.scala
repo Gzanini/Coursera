30f8f54658f0f8cb3cc577c1d2b566f980ea96e6
gzanini <guilhermezsilva@hotmail.com>
2022-06-22 01:33:50 -0300

    W1 - EX2 - Pronto

---
+++ Functional_Programming_Principles_in_Scala/S01_Recfun/src/main/scala/recfun/RecFun.scala
@@ -0,0 +1,44 @@
+package recfun
+
+object RecFun extends RecFunInterface:
+
+  def main(args: Array[String]): Unit =
+    println("Pascal's Triangle")
+    for row <- 0 to 10 do
+      for col <- 0 to row do
+        print(s"${pascal(col, row)} ")
+      println()
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = {
+   if (c == 0 || c == r) 1
+   else pascal(c-1,r-1) + pascal(c,r-1)
+  }
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean =
+    def auxBalance(chars: List[Char], sum: Int): Int =
+      if(chars.isEmpty || sum < 0) sum
+      else if(chars.head == '(') auxBalance(chars.tail, sum + 1)
+      else if(chars.head == ')') auxBalance(chars.tail, sum - 1)
+      else auxBalance(chars.tail, sum)
+
+    auxBalance(chars,0) == 0
+
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int =
+    def auxCountChange(money: Int, coins: List[Int]): Int =
+      if(money == 0) 1
+      else if(coins.isEmpty) 0
+      else if(money < 0 && !coins.isEmpty) 0
+      else auxCountChange(money - coins.head, coins) + auxCountChange(money, coins.tail)
+      
+    if(coins.isEmpty || money <= 0) 0
+    else auxCountChange(money,coins)
---
+++ Functional_Programming_Principles_in_Scala/S01_Recfun/src/main/scala/recfun/untitled.sc
@@ -0,0 +1,14 @@
+
+
+
+val teste = "())("
+
+val teste2 = teste.toList
+
+teste2.isEmpty
+
+teste2.head
+
+teste2.tail
+
+teste2.sum
